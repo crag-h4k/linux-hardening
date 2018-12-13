bd232add17301aa1459a045f3635f1ca0218ed78
deadlift1226 <danemorgan91@gmail.com>
2018-12-13 11:08:31 -0700

    ssh throttle

--- ssh_limiting.sh
+++ ssh_limiting.sh
@@ -1,2 +1,21 @@
-/usr/sbin/iptables -I INPUT -p tcp --dport 22 -i eth0 -m state --state NEW -m recent --set
-/usr/sbin/iptables -I INPUT -p tcp --dport 22 -i eth0 -m state --state NEW -m recent  --update --seconds 60 --hitcount 4 -j DROP
+#!/usr/bin/bash
+#must run as root
+IFACE='eth0'
+
+DPORT=22
+SECONDS=60
+HITS=4
+
+iptables -L
+
+read -p "These are the current rules do you want to continue? (y?)" -n 1 -r
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        iptables -I INPUT -p tcp --dport $DPORT -i $IFACE -m state --state NEW -m recent --set
+        iptables -I INPUT -p tcp --dport $DPORT -i $IFACE -m state --state NEW -m recent  --update --seconds $SECONDS --hitcount $HITS -j DROP
+
+        iptables-save > /etc/iptables-rules
+        iptables-restore < /etc/iptables-rules
+fi
+
