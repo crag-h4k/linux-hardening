b6e4590cd9fb9d60c83b23a8acb42b7ac77aae01
Dane Morgan <32076156+deadlift1226@users.noreply.github.com>
2019-02-16 06:34:41 -0700

    Update quick-ssh.sh

--- quick-ssh.sh
+++ quick-ssh.sh
@@ -1,1 +1,1 @@
-curl https://raw.githubusercontent.com/deadlift1226/linux-hardening/master/sshd_config >> /etc/ssh/sshd_config && systemctl restart sshd;
+curl https://raw.githubusercontent.com/deadlift1226/linux-hardening/master/sshd_config >> /etc/ssh/sshd_config
